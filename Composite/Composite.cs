dcf4cbf70c73693333936223e0b4f32fd3255faa
Furqat <89734235+Furqat-DevDo@users.noreply.github.com>
2024-02-14 18:29:19 +0500

    Composite pattern

---
+++ Composite/Composite.cs
@@ -0,0 +1,41 @@
+﻿namespace Composite;
+// Класс Контейнер содержит сложные компоненты, которые могут иметь
+// вложенные компоненты. Обычно объекты Контейнеры делегируют фактическую
+// работу своим детям, а затем «суммируют» результат.
+
+public class Composite : Component
+{
+    protected readonly List<Component> _children = new();
+        
+    public override void Add(Component component)
+    {
+        _children.Add(component);
+    }
+
+    public override void Remove(Component component)
+    {
+        _children.Remove(component);
+    }
+
+    // Контейнер выполняет свою основную логику особым образом. Он проходит
+    // рекурсивно через всех своих детей, собирая и суммируя их результаты.
+    // Поскольку потомки контейнера передают эти вызовы своим потомкам и так
+    // далее,  в результате обходится всё дерево объектов.
+    public override string Operation()
+    {
+        int i = 0;
+        string result = "Branch(";
+
+        foreach (Component component in _children)
+        {
+            result += component.Operation();
+            if (i != _children.Count - 1)
+            {
+                result += "+";
+            }
+            i++;
+        }
+            
+        return result + ")";
+    }
+}
---
+++ Composite/Program.cs
@@ -0,0 +1,30 @@
+﻿using Composite;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Client client = new Client();
+
+        // Таким образом, клиентский код может поддерживать простые
+        // компоненты-листья...
+        Leaf leaf = new Leaf();
+        Console.WriteLine("Client: I get a simple component:");
+        client.ClientCode(leaf);
+
+        // ...а также сложные контейнеры.
+        Composite.Composite tree = new Composite.Composite();
+        Composite.Composite branch1 = new Composite.Composite();
+        branch1.Add(new Leaf());
+        branch1.Add(new Leaf());
+        Composite.Composite branch2 = new Composite.Composite();
+        branch2.Add(new Leaf());
+        tree.Add(branch1);
+        tree.Add(branch2);
+        Console.WriteLine("Client: Now I've got a composite tree:");
+        client.ClientCode(tree);
+
+        Console.Write("Client: I don't need to check the components classes even when managing the tree:\n");
+        client.ClientCode2(tree, leaf);
+    }
+}
